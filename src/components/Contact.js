a3a4b413387aef78317310a32075a12c7084447f
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2024-11-11 23:10:01 +0500

    some changes in contact us component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,8 +4,7 @@ const Contact = () => {
   return (
     <header className="text-center py-16 bg-coolColor h-auto font-sans">
       <h1 className="text-4xl font-bold text-darkBlue">Contact Us</h1>
-      <p className="text-black mt-8 px-4 sm:px-6 md:px-10 lg:px-20 mx-auto max-w-3xl text-base md:text-lg leading-relaxed">
-      
+      <p className="text-black px-4 sm:px-6 md:px-8 lg:px-16 xl:px-24 my-10 text-justify text-base sm:text-lg md:text-xl">
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since
         the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
         but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
