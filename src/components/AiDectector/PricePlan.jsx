48598e438d31b77298031aba76afaf490ce2b9ab
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2024-11-21 02:26:17 +0500

    add responsiveness

--- src/components/AiDectector/PricePlan.jsx
+++ src/components/AiDectector/PricePlan.jsx
@@ -2,16 +2,13 @@ import React from 'react'
 
 const PricePlan = () => {
   return (
-   
-       <header className="text-center h-auto font-[Poppins] max-w-full mx-auto">
- <div className="flex items-center shadow-lg justify-center h-[137px] bg-[#E7EAF0]">
-        <h1 className="text-4xl font-bold text-[#00159B] drop-shadow-xl">
+    <header className="text-center h-auto font-[Poppins] max-w-full mx-auto">
+      <div className="flex items-center shadow-lg justify-center h-[137px] bg-[#E7EAF0] px-4 sm:px-6 md:px-8">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-[#00159B] drop-shadow-xl">
           Pricing Plans
         </h1>
       </div>
-
-       </header>
-   
+    </header>
   )
 }
 
